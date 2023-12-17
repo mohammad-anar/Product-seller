011cac46b956696cf36c8d3746d9105dfa1bbd4e
Mohammad Anar <ai.anarul4232@gmail.com>
2023-12-18 01:50:30 +0600

    tanstack query installed

--- src/Pages/Carts/Carts.jsx
+++ src/Pages/Carts/Carts.jsx
@@ -1,5 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure/axiosSecure";
 
 const Carts = () => {
+    const axiosSecure = useAxiosSecure();
+    const {data} = useQuery({
+        queryKey:["carts"],
+        queryFn: async () => {
+           const res = await axiosSecure.get("/carts");
+           return res;
+        }
+    })
+    console.log(data);
     return (
         <div>
             cart
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import Routers from "./Routes/Routers.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="max-w-[1400px] mx-auto min-h-screen">
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={Routers}></RouterProvider>
+    </QueryClientProvider>
     </div>
   </React.StrictMode>
 );
