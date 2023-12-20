de7926eb8bc6902a041ce3006709497e73645ec6
Mohammad Anar <ai.anarul4232@gmail.com>
2023-12-20 17:04:42 +0600

    axios interceptors complete

--- src/hooks/useAxiosSecure/AxiosSecure.jsx
+++ src/hooks/useAxiosSecure/AxiosSecure.jsx
@@ -1,9 +1,42 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001",
+  baseURL: "http://localhost:5001",
 });
 const useAxiosSecure = () => {
+  // axios interceptors  request
+  // instance.interceptors.request.use(
+  //   (config) => {
+  //     const token = localStorage.getItem("token");
+  //     config.headers.authorization = `Bearer ${token}`;
+  //     return config;
+  //   },
+  //   (error) => {
+  //     return Promise.reject(error);
+  //   }
+  // );
+  instance.interceptors.request.use((config) => {
+    //set token to req config header
+    const token = localStorage.getItem("token");
+    config.headers.authorization = `Bearer ${token}`
+    return config
+ }, (error) => {
+    return Promise.reject(error)
+ });
+  // axios interceptors  respnse
+  instance.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      const status = error.response.status;
+      if (status === 401 || status === 403) {
+        console.log(error);
+      }
+      return Promise.reject(error);
+    }
+  );
+
   return instance;
 };
 
